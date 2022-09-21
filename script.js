8eb9a5c867c39850e7811039d64084d74e2b7bac
Ryan Choi <rychoi42@gmail.com>
2022-09-21 16:27:17 -0700

    Add helper getValidInput(), game() function

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 "use strict";
 
-let playerChoice = null;
-while (typeof(playerChoice) !== 'string') {
-    playerChoice = prompt('Choice: ', '');
-}
-alert(playRound(playerChoice, getComputerChoice()));
+game();
 
 function getComputerChoice() {
     return numberToChoice(Math.floor(Math.random() * 3) + 1);
@@ -28,20 +24,42 @@ function numberToChoice(num) {
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
-        if (playerSelection === 'rock' && computerSelection !== 'rock') {
-            return computerSelection === 'paper' ? 'You lose! Paper beats rock.' :
-            'You win! Rock beats scissors.';
-        }
-        else if (playerSelection === 'paper' && computerSelection !== 'paper') {
-            return computerSelection === 'scissors' ? 'You lose! Scissors beats paper' :
-            'You win! Paper beats rock';
-        }
-        else if (playerSelection === 'scissors' && computerSelection !== 'scissors') {
-            return computerSelection === 'rock' ? 'You lose! Rock beats scissors' : 
-            'You win! Scissors beats paper';
-        }
-        return 'It\'s a tie!';
+    if (playerSelection === 'rock' && computerSelection !== 'rock') {
+        return computerSelection === 'paper' ? 'You lose! Paper beats rock.' :
+        'You win! Rock beats scissors.';
     }
-    return 'Invalid input';
+    else if (playerSelection === 'paper' && computerSelection !== 'paper') {
+        return computerSelection === 'scissors' ? 'You lose! Scissors beats paper' :
+        'You win! Paper beats rock';
+    }
+    else if (playerSelection === 'scissors' && computerSelection !== 'scissors') {
+        return computerSelection === 'rock' ? 'You lose! Rock beats scissors' : 
+        'You win! Scissors beats paper';
+    }
+    return 'It\'s a tie!';
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = getValidInput();
+        if (playerChoice === null) {
+            console.log('Cancelled game');
+            return null;
+        }
+        console.log(playRound(playerChoice, getComputerChoice()));
+    }
+}
+
+function getValidInput() {
+    let playerChoice = null;
+    do {
+        playerChoice = prompt('Choice:', '');
+        if (playerChoice === null) {
+            return playerChoice;
+        }
+        if (typeof(playerChoice) === 'string') {
+            playerChoice = playerChoice.toLowerCase();
+        }
+    } while(playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors');
+    return playerChoice;
 }
