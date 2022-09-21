cdb52605619d19270c43841050e998138d2f1a60
Ryan Choi <rychoi42@gmail.com>
2022-09-21 15:49:20 -0700

    Finish playRound() function
    Add safeguards for invalid/non-string inputs

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 "use strict";
+
+let playerChoice = null;
+while (typeof(playerChoice) !== 'string') {
+    playerChoice = prompt('Choice: ', '');
+}
+alert(playRound(playerChoice, getComputerChoice()));
+
 function getComputerChoice() {
     return numberToChoice(Math.floor(Math.random() * 3) + 1);
 }
@@ -6,15 +13,35 @@ function getComputerChoice() {
 function numberToChoice(num) {
     switch (num) {
         case 1: 
-            return 'Rock';
+            return 'rock';
             break;
         case 2: 
-            return 'Paper';
+            return 'paper';
             break;
         case 3: 
-            return 'Scissors';
+            return 'scissors';
             break;
         default: 
             return 'Invalid input.';
     }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+        if (playerSelection === 'rock' && computerSelection !== 'rock') {
+            return computerSelection === 'paper' ? 'You lose! Paper beats rock.' :
+            'You win! Rock beats scissors.';
+        }
+        else if (playerSelection === 'paper' && computerSelection !== 'paper') {
+            return computerSelection === 'scissors' ? 'You lose! Scissors beats paper' :
+            'You win! Paper beats rock';
+        }
+        else if (playerSelection === 'scissors' && computerSelection !== 'scissors') {
+            return computerSelection === 'rock' ? 'You lose! Rock beats scissors' : 
+            'You win! Scissors beats paper';
+        }
+        return 'It\'s a tie!';
+    }
+    return 'Invalid input';
 }
