3153c87c4ef4e877ae15c8a0e7de6576e515659c
Ryan Choi <rychoi42@gmail.com>
2022-09-28 09:46:37 -0700

    Finish loading/copyrights
    Add images, fonts, loading/toggling functions
    START!

---
+++ script/script.js
@@ -0,0 +1,151 @@
+"use strict";
+
+let playerWins = 0;
+let computerWins = 0;
+let gameStatus = 'N/A';
+
+window.addEventListener('load', () => {
+    const page = document.querySelector('body');
+    page.classList.toggle('white');
+    page.classList.toggle('transition');
+});
+const copyrights = document.querySelector('.copyrights');
+//setTimeout(disappear(copyrights), 3000);
+//disappear(copyrights);
+//gameStart();
+
+function disappear(element) {
+    element.classList.toggle('hidden');
+}
+
+function getComputerChoice() {
+    return numberToChoice(Math.floor(Math.random() * 3) + 1);
+}
+
+function numberToChoice(num) {
+    switch (num) {
+        case 1: 
+            return 'rock';
+            break;
+        case 2: 
+            return 'paper';
+            break;
+        case 3: 
+            return 'scissors';
+            break;
+        default: 
+            return 'Invalid input.';
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    const roundResult = document.querySelector('.round-result');
+    if (playerSelection === 'rock' && computerSelection !== 'rock') {
+        if (computerSelection === 'paper') {
+            computerWins++;
+            roundResult.textContent = 'Beep boop, paper beats rock';
+        }
+        else {
+            playerWins++;
+            roundResult.textContent = 'SMASH.';
+        }
+    }
+    else if (playerSelection === 'paper' && computerSelection !== 'paper') {
+        if (computerSelection === 'scissors') {
+            computerWins++;
+            roundResult.textContent = 'sudo apt snip-snip';
+        }
+        else {
+            playerWins++;
+            roundResult.textContent = 'Wrap, not rap.';
+        }
+    }
+    else if (playerSelection === 'scissors' && computerSelection !== 'scissors') {
+        if (computerSelection === 'rock') {
+            computerWins++;
+            roundResult.textContent = 'Computer, rock beats scissors.';
+        }
+        else {
+            playerWins++;
+            roundResult.textContent = 'You\'re sharp, scissors cuts through paper.';
+        }
+    }
+    else {
+        roundResult.textContent = 'Even exchange gentle-machmen';
+    }
+}
+
+function gameStart() {
+    const replay = document.querySelector('.replay');
+    const result = document.querySelector('.result');
+    replay.textContent = '';
+    playerWins = 0;
+    computerWins = 0;
+    gameStatus = 'N/A';
+    const buttonClick = document.querySelectorAll('button');
+    buttonClick.forEach(button => {
+        button.addEventListener('click', () => {
+            const choice = button.className;
+            playRound(choice, getComputerChoice());
+            keepScore(playerWins, computerWins);
+            if (playerWins === 5 || computerWins === 5) {
+                gameEnd();
+            }
+            console.log(gameStatus, playerWins, computerWins);
+        })
+    });
+}
+
+function gameEnd() {
+    const buttonClick = document.querySelectorAll('button');
+    buttonClick.forEach(button => {
+        button.replaceWith(button.cloneNode(true));
+    })
+    if (playerWins > computerWins) {
+        gameStatus = 'W';
+    }
+    else if (playerWins < computerWins) {
+        gameStatus = 'L';
+    }
+    else {
+        gameStatus = 'T';
+    }
+    printWinner();
+}
+
+function keepScore(playerWins, computerWins) {
+    const content = document.querySelector('.game');
+    const player_score = document.querySelector('.player_score');
+    const computer_score = document.querySelector('.computer_score');
+    player_score.textContent = playerWins;
+    computer_score.textContent = computerWins;
+    if (playerWins === 5 || computerWins === 5) {
+        gameEnd();
+    }
+}
+
+function re() {
+    const result = document.querySelector('.result');   
+    const replay = document.querySelector('.replay');
+    if (gameStatus === 'W') {
+        result.textContent = 'You WON!';
+        replay.textContent = 'Go for the back-to-back?';
+    }
+    else if (gameStatus === 'L') {
+        result.textContent = 'You LOSE!';
+        replay.textContent = 'Reclaim your honour?';
+    }
+    else {
+        result.textContent = 'TIE!';
+        replay.textContent = 'B-O-R-I-N-G, Go again?';
+    }
+}
+
+function restartGame() {
+    playerWins = 0;
+    computerWins = 0;
+    gameStatus = 'N/A';
+    const replay = document.querySelector('.replay');
+    replay.addEventListener('click', gameStart());
+}
+
